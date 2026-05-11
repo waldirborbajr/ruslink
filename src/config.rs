cfdcf3e3152ed7d3b5a24df18d8104c984e8da9c
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-05-11 14:28:38 -0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,27 @@
+// src/config.rs
 use std::path::PathBuf;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Config {
     pub package: String,
     pub stow_dir: PathBuf,
     pub target_dir: PathBuf,
+
+    // Operation modes
     pub delete: bool,
     pub restow: bool,
     pub dry_run: bool,
+
+    // Output
     pub verbose: bool,
+
+    // Git
     pub auto_git: bool,
     pub git_push: bool,
+    pub commit_message: Option<String>,
+
+    // Conflict handling
     pub force: bool,
     pub backup: bool,
     pub adopt: bool,
-    pub commit_message: Option<String>,
 }
